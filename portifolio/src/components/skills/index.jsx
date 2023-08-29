5a8b2b262a34a235cbc96e84ea3ac4cafddc7408
Lucas Castelan <lucascastelanph2@gmail.com>
2023-08-29 08:30:26 -0300

    "att repo"

--- portifolio/src/components/skills/index.jsx
+++ portifolio/src/components/skills/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import skillsData from "./skiils.json";
 import "../skills/skills.sass";
 import Card from "../Card";
 
@@ -7,11 +6,22 @@ export default function Skills() {
   return (
     <div className="container-pai">
       <div className="container-skills">
-        {skillsData.skills.map((skill) => (
-          <div className="conteudo-skills" key={skill.nome}>
-            <Card nome={skill.nome} src={skill.src} />
-          </div>
-        ))}
+        <div className="conteudo-skills">
+          <Card nome1={"HTML5"} src={"../public/img/Html.svg"} />
+          <Card nome2={"REACT"} src="../public/img/react.svg" />
+        </div>
+        <div className="conteudo-skills">
+          <Card nome3={"CSS3"} src3="../public/img/css.svg" />
+          <Card nome4={"TAILWIND"} src4="../public/img/tw2.png" />
+        </div>
+        <div className="conteudo-skills">
+          <Card nome5={"SASS"} src5="../public/img/sass.svg" />
+          <Card nome6={"GIT"} src6="../public/img/git.svg" />
+        </div>
+        <div className="conteudo-skills">
+          <Card nome7={"J.SCRIPT"} src7="../public/img/js.svg" />
+          <Card nome8={"FIGMA"} src8="../public/img/figma.svg" />
+        </div>
       </div>
     </div>
   );
